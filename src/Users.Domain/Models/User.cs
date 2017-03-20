00aae1bfbec9d0bca43015f437e60fa335be23aa
SondreB <pixelworld@hotmail.com>
2017-03-20 19:04:52 +0100

    Adds Actor service for Player

---
+++ src/Users.Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Domain.Models
+{
+    [DataContract(Name = "organization", Namespace = "http://schemas.webdevkristiansand/2017/03/organization")]
+    public class User : Entity
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
